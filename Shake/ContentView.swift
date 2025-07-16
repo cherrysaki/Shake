31a8044250d33ca785866453a918fd97f16df4e0
Saki <ksaki0402@gmail.com>
2025-07-16 17:17:01 +0900

    Initial Commit

---
+++ Shake/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Shake
+//
+//  Created by 神林沙希 on 2025/07/16.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Shake/ShakeApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShakeApp.swift
+//  Shake
+//
+//  Created by 神林沙希 on 2025/07/16.
+//
+
+import SwiftUI
+
+@main
+struct ShakeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
